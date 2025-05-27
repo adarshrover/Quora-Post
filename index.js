eedb0c56c279d78f631177c80323eebe01a18974
Adarsh Kumar <160909822+adarshrover@users.noreply.github.com>
2025-05-27 22:58:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 8080;
+const PORT = process.env.PORT || 3000;
 const path = require('path');
 
 const {v4: uuidv4} = require('uuid');
